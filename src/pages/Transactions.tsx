8e2fda27c4444de5f0b2d1f19930f312559b9b28
rafael-correa98 <rafaelcorrean98@gmail.com>
2022-09-30 21:07:14 -0300

    login

---
+++ src/pages/Transactions.tsx
@@ -0,0 +1,3 @@
+export default function Transactions() {
+	return <h1>Transaction</h1>;
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login } from "./pages/Login";
-import { Signup } from "./pages/Signup";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Signup } from './pages/Signup';
+import Transactions from './pages/Transactions';
 
 export const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/transactions" element={<Transactions />} />
+				<Route path="*" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
